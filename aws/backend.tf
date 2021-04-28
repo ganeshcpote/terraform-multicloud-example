b3d62bd9b86f3b977f6c4383429d2bb31d23bc15
Ganesh Pote <56916550+ganeshcpote@users.noreply.github.com>
2021-04-28 22:05:35 +0530

    Update backend.tf

--- aws/backend.tf
+++ aws/backend.tf
@@ -1,12 +1,12 @@
 terraform {
-  backend "local" {}
+  backend "s3" {}
 }
 
-#data "terraform_remote_state" "state" {
-#  backend = "s3"
-#  config = {
-#    bucket     = "${var.tf_state_bucket}"
-#    region     = "${var.aws_region}"
-#    key        = "aws_deployment/devops"
-#  }
-#}
+data "terraform_remote_state" "state" {
+  backend = "s3"
+  config = {
+    bucket     = "${var.tf_state_bucket}"
+    region     = "${var.aws_region}"
+    key        = "tf-state"
+  }
+}
