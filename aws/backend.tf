0312347ffa35f068819ff85c4694e64ad8a2f837
Ganesh Pote <56916550+ganeshcpote@users.noreply.github.com>
2021-05-03 16:32:23 +0530

    Update backend.tf

--- aws/backend.tf
+++ aws/backend.tf
@@ -3,10 +3,3 @@ terraform {
     conn_str    = "postgres://jenkins:jenkins@localhost/terraformstate?sslmode=disable"
     }
 }
-
-data "terraform_remote_state" "aws_deployment" {
-  backend = "pg"
-  config = {
-    conn_str    = "postgres://jenkins:jenkins@localhost/terraformstate?sslmode=disable"
-  }
-}
