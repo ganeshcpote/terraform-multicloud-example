95b747295388741a72ac88d9e639bf009931b499
ganesh.pote@hotmail.com <ganesh.pote@hotmail.com>
2021-05-04 18:58:19 +0530

    Signed-off-by: ganesh.pote@hotmail.com <ganesh.pote@hotmail.com>

--- azure/backend.tf
+++ azure/backend.tf
@@ -1,10 +1,5 @@
-#terraform {
-#  backend "pg" {
-#    conn_str    = "postgres://jenkins:jenkins@localhost/terraformstate?sslmode=disable"
-#    }
-#}
-
 terraform {
-  backend "local" {}
+  backend "pg" {
+    conn_str    = "postgres://jenkins:jenkins@localhost/terraformstate?sslmode=disable"
+    }
 }
-
